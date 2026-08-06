c91099b06328c5ea68eb55606343895fda8fdfee
Yuvraj-Malik <malikyuvraj2701@gmail.com>
2026-08-06 12:07:07 +0530

    Fix applied in attempt 1

--- ai-worker/scripts/test-repo/bugs/06-misleading-comment.js
+++ ai-worker/scripts/test-repo/bugs/06-misleading-comment.js
@@ -1,7 +1,8 @@
-// Converts an object of {id, name} to an array of [id, name] pairs.
+// Converts an object of {id, name} to an object of {userId, userName}.
 function formatUser(user) {
-  // Bug: object doesn't have length, map won't work on object.
-  // Comment says "array of [id, name] pairs" but usage in test expects { userId: id, userName: name }.
-  return Object.keys(user).map(k => [k, user[k]]);
+  return {
+    userId: user.id,
+    userName: user.name
+  };
 }
 module.exports = { formatUser };
